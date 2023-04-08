98b7885350fd5b0f47d09c39406c2d6f14f55912
uhanuu <hwryu990927@gmail.com>
2023-04-09 00:07:15 +0900

    refactor : gitignore 수정

---
+++ src/main/java/io/mojolll/project/v1/api/config/jwt/JwtProperties.java
@@ -0,0 +1,10 @@
+package io.mojolll.project.v1.api.config.jwt;
+
+public interface JwtProperties {
+
+    String ACCESS_SECRET = "kem4328";
+    String REFRESH_SECRET = "cojopleProjectWithMinsub";
+    int EXPIRATION_TIME = 3600000;
+    String TOKEN_PREFIX = "Bearer ";
+    String ACCESS_HEADER_STRING = "Authorization";
+}
